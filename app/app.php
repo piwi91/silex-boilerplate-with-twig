a58243e0cc8c80eaabec0664bc3c74358b0a2222
Pim Widdershoven <pim@hotflo.net>
2015-11-24 10:30:15 +0100

    Add Twig implementation

--- app/app.php
+++ app/app.php
@@ -6,8 +6,18 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 $app->get('/', function() {
     return new Response('Welcome to my new Silex app');
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return $app['twig']->render('hello.twig', array(
+        'name' => $name,
+    ));
 });
 
 return $app;
